4b683d3579fa2f579d094b1456000b68fb6378c6
jeffsee55 <jeffsee.55@gmail.com>
2016-07-29 16:22:20 -0400

    Hotfix

--- includes/class-simple-subscriber-form-processor.php
+++ includes/class-simple-subscriber-form-processor.php
@@ -13,6 +13,17 @@
 class Simple_Subscriber_Form_Processor {
 
   public function process_signup_form() {
-    exit( var_dump( $_POST ) );
+    $email = isset($_POST['subscriber_email']) ?: null;
+    if( !$email )
+      die();
+
+    if( user_exists( $email ) ) {
+      add_user_to_blog(  );
+    } else {
+      $userdata = array(
+        'user_email' => $email,
+      );
+      wp_insert_user( $userdata );
+    }
   }
 }
